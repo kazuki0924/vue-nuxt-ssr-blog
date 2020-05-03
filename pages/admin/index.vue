5771783abebc50d5592c0d831f1cb58f46f286ff
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-05-03 16:40:00 +0900

    7

---
+++ pages/admin/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="admin-page">
+    <section class="new-post">
+      <button>Create Post</button>
+    </section>
+    <section class="existing-posts">
+      <h1>Existing Posts</h1>
+      <PostList />>
+    </section>
+  </div>
+</template>
