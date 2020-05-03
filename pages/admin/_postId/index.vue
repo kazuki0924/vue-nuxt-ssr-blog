69b0a63688ef14c7979f793e8789415d00862f1e
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-05-03 19:01:46 +0900

    10

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="admin-post-page">
-    <section class="update-form"></section>
-    <AdminPostForm :post="loadedPost" />
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
   </div>
 </template>
 
@@ -9,19 +10,32 @@
 import AdminPostForm from '@/components/Admin/AdminPostForm'
 
 export default {
+  layout: 'admin',
   components: {
     AdminPostForm
   },
   data() {
     return {
       loadedPost: {
-        author: 'Bob',
+        author: 'Maximilian',
         title: 'My awesome Post',
         content: 'Super amazing, thanks for that!',
-        thumbnailLink:
-          'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
+        thumbnailLink: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
       }
     }
   }
 }
 </script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
