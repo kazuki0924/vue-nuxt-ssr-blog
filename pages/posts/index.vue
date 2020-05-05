6bf64720dd4d1c7982bc535032911586f6894b61
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-05-05 12:39:29 +0900

    16

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -5,49 +5,20 @@
 </template>
 
 <script>
-import PostList from '@/components/Posts/PostList'
+import PostList from "@/components/Posts/PostList";
 
 export default {
   components: {
     PostList
   },
-  asyncData(context) {
-    return new Promise((resolve, reject) => {
-      // eslint-disable-next-line nuxt/no-timing-in-fetch-data
-      setTimeout(() => {
-        resolve({
-          loadedPosts: [
-            {
-              id: '1',
-              title: 'First Post',
-              previewText: 'This is our first post!',
-              thumbnail:
-                'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
-            },
-            {
-              id: '2',
-              title: 'Second Post',
-              previewText: 'This is our second post!',
-              thumbnail:
-                'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
-            }
-          ]
-        })
-      }, 1000)
-      // reject(new Error())
-    })
-      .then((data) => {
-        return data
-      })
-      .catch((e) => {
-        context.error(e)
-      })
-  },
-  created() {
-    this.$store.dispatch('setPosts', this.loadedPosts)
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts
+    }
   }
-}
+};
 </script>
+
 
 <style scoped>
 .posts-page {
